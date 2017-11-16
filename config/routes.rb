3ab9e3e81fbf8c5cd7a85ffae076c30f945ef880
Amanda Wehrman <arwehrman@gmail.com>
2017-11-16 03:22:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
   get '/home' => "welcome#home"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
